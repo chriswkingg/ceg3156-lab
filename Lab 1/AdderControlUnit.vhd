00c2486cfdb1d48dd45a2f62840e752f2aa866fb
Chriswkingg <chriswkingg@gmail.com>
2024-05-14 15:44:35 -0400

    Control logic done

--- Lab 1/AdderControlUnit.vhd
+++ Lab 1/AdderControlUnit.vhd
@@ -26,11 +26,11 @@ COMPONENT dflipflop IS
 END dflipflop;
 );
 END COMPONENT
-SIGNAL int_s0, int_s1, int_s2, int_s3, int_s4, int_s5 : STD_LOGIC; 
+SIGNAL int_s0, int_s1, int_s2, int_s3, int_s4, int_s5, int_s6 : STD_LOGIC; 
 BEGIN
 	s0 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => 0,
 		i_clock => i_clock,
 		i_enable => 1
 		i_async_reset => 0,
@@ -40,55 +40,75 @@ BEGIN
 	);
 	s1 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => int_s0,
 		i_clock => i_clock,
 		i_enable => 1
-		i_async_reset => 0,
-		i_async_set => i_reset,
+		i_async_reset => i_reset,
+		i_async_set => 0,
 		o_q => int_s1,
 		o_qBar => OPEN
 	);
 	s2 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => int_s1 AND NOT i_DownCounterEmpty,
 		i_clock => i_clock,
 		i_enable => 1
-		i_async_reset => 0,
-		i_async_set => i_reset,
+		i_async_reset => i_reset,
+		i_async_set => 0,
 		o_q => int_s2,
 		o_qBar => OPEN
 	);
 	s3 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => (int_s1 OR int_s2) AND i_downCounterEmpty,
 		i_clock => i_clock,
 		i_enable => 1
-		i_async_reset => 0,
-		i_async_set => i_reset,
+		i_async_reset => i_reset,
+		i_async_set => 0,
 		o_q => int_s3,
 		o_qBar => OPEN
 	);
 	s4 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => int_s3 AND i_mantissaCarry,
 		i_clock => i_clock,
 		i_enable => 1
-		i_async_reset => 0,
-		i_async_set => i_reset,
+		i_async_reset => i_reset,
+		i_async_set => 0,
 		o_q => int_s4,
 		o_qBar => OPEN
 	);
 	s5 : dflipflop
 	PORT MAP(
-		i_d => ,
+		i_d => (int_s3 OR int_s5) AND NOT i_mantissaSumMSB,
 		i_clock => i_clock,
 		i_enable => 1
-		i_async_reset => 0,
-		i_async_set => i_reset,
+		i_async_reset => i_reset,
+		i_async_set => 0,
 		o_q => int_s5,
 		o_qBar => OPEN
 	);
-	
-	
+	s6 : dflipflop
+	PORT MAP(
+		i_d => int_s4 OR (int_s5 AND i_mantissaSumMSB),
+		i_clock => i_clock,
+		i_enable => 1
+		i_async_reset => i_reset,
+		i_async_set => 0,
+		o_q => int_s6,
+		o_qBar => OPEN
+	);
+	o_loadA <= int_s0;
+	o_loadB <= int_s0;
+	o_loadDownCounter <= int_s1;
+	o_decrementDownCounter <= int_s2;
+	o_smallerMantissaLeftShift <= int_s2;
+	o_loadSumE <= int_s3;
+	o_loadSumM <= int_s3;
+	o_loadSumS <= int_s3; 
+	o_rightShiftSum <= int_s4;
+	o_incrementSumExponent <= int_s4;
+	o_leftShiftSum <= int_s5; 
+	o_decrementSumExponent <= int_s5; 
 	
 END ARCHITECTURE;
