b88ff020ab0758e1ece09e5e602fdc38c197330e
Josue Dazogbo <jdazo054@uottawa.ca>
2024-05-14 21:29:15 -0400

    Completed DataPath Implementation

--- Lab 1/AdderDataPath.vhd
+++ Lab 1/AdderDataPath.vhd
@@ -17,15 +17,31 @@ ENTITY AdderDataPath is
         --Control Path Links
         SHFTM                           :   IN STD_LOGIC;
         LDDC, DECDC                     :   IN STD_LOGIC;
+        LDAM, LDBM                      :   IN STD_LOGIC;
+        LDSM, LSHFTM, RSHFTM            :   IN STD_LOGIC;
+        LDSE, INCSE, DECSE              :   IN STD_LOGIC;
+        CLRS, LDAS                      :   IN STD_LOGIC;
     );
     END AdderDataPath;
 
 ARCHITECTURE rtl of AdderDataPath is
 
     SIGNAL SHFTAM, SHFTBM, AEGTBE, DCEMT    : STD_LOGIC;
-    SIGNAL adder1_result, adder2_result     : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL exponentSubtractor_result        : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL exponentDifferenceNegator_result : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL ExponentA_8Bit, ExponentB_8Bit   : STD_LOGIC_VECTOR(6 downto 0);
+    SIGNAL MantissaA_9Bit, MantissaB_9Bit   : STD_LOGIC_VECTOR(8 downto 0);
     SIGNAL downcounter_result               : STD_LOGIC_VECTOR(7 downto 0);
     SIGNAL register_Am_result               : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL register_Bm_result               : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL register_Am_negator_result       : STD_LOGIC_VECTOR(8 downto 0);
+    SIGNAL register_Bm_negator_result       : STD_LOGIC_VECTOR(8 downto 0);
+    SIGNAL mantissa_adder                   : STD_LOGIC_VECTOR(8 downto 0);
+    SIGNAL mantissa_adder_8Bit              : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL register_Sm_result               : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL larger_exponent                  : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL register_Se_result               : STD_LOGIC_VECTOR(7 downto 0);
+    SIGNAL register_Sum_input               : STD_LOGIC_VECTOR(15 downto 0);
 
     COMPONENT EightBitComparator IS
         PORT(
@@ -33,103 +49,131 @@ ARCHITECTURE rtl of AdderDataPath is
             o_GT, o_LT, o_EQ		: OUT	STD_LOGIC
         );
         END COMPONENT;
-    
-    COMPONENT AdderSubtractor is 
+
+    COMPONENT EightBitAdderSubtractor
         PORT
         (
-            InputA, InputB  :   IN STD_LOGIC_VECTOR(3 downto 0);
-            Operation       :   IN STD_LOGIC;
-            Result          :   OUT STD_LOGIC_VECTOR(3 downto 0);
-            CarryOUT        :   OUT STD_LOGIC
+            InputA, InputB  : IN STD_LOGIC_VECTOR(7 downto 0);
+            Operation   :   IN STD_LOGIC;
+            Result    :   OUT STD_LOGIC_VECTOR(7 downto 0);
+            CarryOUT    :   OUT STD_LOGIC
         );
         END COMPONENT;
     
-    COMPONENT EightBitDecrementer IS
-        PORT(
-            i_resetBar, i_load, i_dec    : IN    STD_LOGIC;
-            i_clock                      : IN    STD_LOGIC;
-            i_Value                      : IN    STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_Value                      : OUT    STD_LOGIC_VECTOR(7 DOWNTO 0)
-        );
-        END COMPONENT;
+    COMPONENT EightBitGPRegister
+        PORT 
+        (
+            -- Register Operations
+            i_resetBar : IN STD_LOGIC;
+            i_load, i_shiftLeft, i_shiftRight : IN STD_LOGIC;
+            i_decrement, i_increment : IN STD_LOGIC;
 
-    COMPONENT EightBitLeftShiftRegister IS
-        PORT (
-            i_resetBar, i_load, i_shift : IN STD_LOGIC;
+            -- Register Signals
+            i_serial_in_left, i_serial_in_right : IN STD_LOGIC;
             i_clock : IN STD_LOGIC;
-            i_shift_entry : IN STD_LOGIC;
-            o_shift_out   : OUT STD_LOGIC;
             i_Value : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
             o_Value : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
         );
-        END COMPONENT;
+    END COMPONENT;
 
-    COMPONENT EightBitRightShiftRegister IS
-        PORT (
-            i_resetBar, i_load, i_shift : IN STD_LOGIC;
-            i_clock                     : IN STD_LOGIC;
-            i_shift_entry               : IN STD_LOGIC;
-            o_shift_out                 : OUT STD_LOGIC;
-            i_Value                     : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_Value                     : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+
+    
+    COMPONENT NineBitAdderSubtractor
+        PORT
+        (
+            InputA, InputB  : IN STD_LOGIC_VECTOR(8 downto 0);
+            Operation   :   IN STD_LOGIC;
+            Result    :   OUT STD_LOGIC_VECTOR(8 downto 0);
+            CarryOUT    :   OUT STD_LOGIC
         );
         END COMPONENT;
+    
+    COMPONENT NineBitGPRegister
+        PORT 
+        (
+            -- Register Operations
+            i_resetBar : IN STD_LOGIC;
+            i_load, i_shiftLeft, i_shiftRight : IN STD_LOGIC;
+            i_decrement, i_increment : IN STD_LOGIC;
 
-    COMPONENT EightBitRegister IS
-        PORT (
-            i_resetBar, i_load, i_clock : IN    STD_LOGIC;
-            i_Value                     : IN    STD_LOGIC_VECTOR(7 DOWNTO 0);
-            o_Value                     : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0)
+            -- Register Signals
+            i_serial_in_left, i_serial_in_right : IN STD_LOGIC;
+            i_clock : IN STD_LOGIC;
+            i_Value : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+            o_Value : OUT STD_LOGIC_VECTOR(8 DOWNTO 0)
         );
-        END COMPONENT;
+    END COMPONENT;
+
+    COMPONENT TwoToOne8BitMux
+        PORT (
+            i_muxIn0, i_muxIn1	: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            o_mux				: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            sel					: IN STD_LOGIC
+        );
+    END COMPONENT;
+
+    COMPONENT mux_2to1_top
+        Port ( SEL : in  STD_LOGIC;
+            A   : in  STD_LOGIC;
+            B   : in  STD_LOGIC;
+            X   : out STD_LOGIC);
+    END COMPONENT;
 
     BEGIN
 
     SHFTAM <= SHFTM and AEGTBE;
     SHFTBM <= SHFTM and (not AEGTBE);
 
-    comp1   :   EightBitComparator
+    ExponentA_8Bit <= (7 => '0', 6 downto 0 => ExponentA);
+    ExponentB_8Bit <= (7 => '0', 6 downto 0 => ExponentB);
+    MantissaA_9Bit <= (8 => '1', 7 downto 0 => MantissaA);
+    MantissaB_9Bit <= (8 => '1', 7 downto 0 => MantissaB);
+
+    mantissa_adder_8Bit <=  (7 downto 0 => mantissa_adder(7 downto 0));
+
+
+    exponentComparator   :   EightBitComparator
         PORT MAP
         (
-            i_Ai => ExponentA,
-            i_Bi => ExponentB,
+            i_Ai => ExponentA_8Bit,
+            i_Bi => ExponentB_8Bit,
             o_GT => AEGTBE,
             o_LT => open,
             o_EQ => open
         );
 
-    adder1  :   AdderSubtractor
+    exponentSubtractor  :   EightBitAdderSubtractor
         PORT MAP
         (
-            InputA => ExponentA,
-            InputB => ExponentB,
-            Operation => '1',
-            Result => adder1_result,
+            InputA => ExponentA_8Bit,
+            InputB => ExponentB_8Bit,
+            Operation => '1', -- Which means subtract
+            Result => exponentSubtractor_result,
             CarryOUT => open
         );
 
-    adder2  :   AdderSubtractor
+    exponentDifferenceNegator  :   EightBitAdderSubtractor
         PORT MAP
         (
-            InputA => adder1_result,
+            InputA => exponentSubtractor_result,
             InputB => "00000000",
             Operation => AEGTBE,
-            Result => adder2_result,
+            Result => exponentDifferenceNegator_result,
             CarryOUT => open
         );
 
-    downcounter     :   EightBitDecrementer
+    downcounter     :   NineBitGPRegister
         PORT
         (
             i_resetBar => GReset,
             i_load => LDDC,
             i_dec => DECDC,
             i_clock => GClock,
-            i_Value => adder2_result,
+            i_Value => exponentDifferenceNegator_result,
             o_Value => ,
         );
 
-    comp2   :   EightBitComparator
+    downcounterEmptyComparator   :   EightBitComparator
         PORT MAP
         (
             i_Ai => downcounter_result,
@@ -139,23 +183,128 @@ ARCHITECTURE rtl of AdderDataPath is
             o_EQ => DCEMT
         );
 
-    register_Am     :   EightBitLeftShiftRegister
+    register_Am     : NineBitGPRegister
         PORT MAP
         (
+        
             i_resetBar => GReset,
-            i_load => LDAM,
+            i_load => LDAM, 
+            i_shiftLeft => SHFTAM,
+            i_shiftRight => open,
+            i_decrement => open,
+            i_increment => open,
+            i_serial_in_left => open,
+            i_serial_in_right => '0',
             i_clock => GClock,
-            i_shift_entry => '0',
-            o_Value => register_Am_result,
+            i_Value => MantissaA_9Bit,
+            o_Value => register_Am_result
+
+        );  
+    
+    register_Bm     : NineBitGPRegister
+        PORT MAP
+        (
+        
+            i_resetBar => GReset,
+            i_load => LDBM, 
+            i_shiftLeft => open,
+            i_shiftRight => SHFTBM,
+            i_decrement => open,
+            i_increment => open,
+            i_serial_in_left => '0',
+            i_serial_in_right => open,
+            i_clock => GClock,
+            i_Value => MantissaB_9Bit,
+            o_Value => register_Bm_result
+
+        ); 
+    
+    register_Am_negator  :   NineBitAdderSubtractor
+        PORT MAP
+        (
+            InputA => "000000000",
+            InputB => MantissaA_9Bit,
+            Operation => SignA, 
+            Result => register_Am_negator_result,
+            CarryOUT => open
+        );
+
+    register_Bm_negator  :   NineBitAdderSubtractor
+        PORT MAP
+        (
+            InputA => "000000000",
+            InputB => MantissaB_9Bit,
+            Operation => SignB,
+            Result => register_Bm_negator_result,
+            CarryOUT => open
         );
     
-    register_Bm     :   EightBitLeftShiftRegister
+        mantissa_adder  :   NineBitAdderSubtractor
         PORT MAP
         (
-            i_resetBar => GReset,
-            i_load => LDAM,
-            i_clock => GClock, 
-            i_shift_entry => '0',
-            o_Value => register_Am_result,
+            InputA => register_Am_negator_result,
+            InputB => register_Bm_negator_result,
+            Operation => '0',
+            Result => mantissa_adder_result,
+            CarryOUT => open
         );
+    
+    register_Sm          : EightBitGPRegister
+        PORT MAP
+        (
+        
+            i_resetBar => GReset,
+            i_load => LDSM, 
+            i_shiftLeft => LSHFTM,
+            i_shiftRight => RSHFTM,
+            i_decrement => open,
+            i_increment => open,
+            i_serial_in_left => '0',
+            i_serial_in_right => '0',
+            i_clock => GClock,
+            i_Value => mantissa_adder_8Bit,
+            o_Value => register_Bm_result
+
+        );
+    
+    expoenntChoser      : TwoToOne8BitMux
+        PORT MAP
+        (
+            i_muxIn0 => ExponentB,
+            i_muxIn1 => ExponentA,
+            o_mux => larger_exponent,
+            sel	=> AEGTBE
+        );
+    
+    register_Se          : EightBitGPRegister
+        PORT MAP
+        (
+        
+            i_resetBar => GReset,
+            i_load => LDSE, 
+            i_shiftLeft => open,
+            i_shiftRight => open,
+            i_decrement => INCSE,
+            i_increment => DECSE,
+            i_serial_in_left => open,
+            i_serial_in_right => open,
+            i_clock => GClock,
+            i_Value => larger_exponent,
+            o_Value => register_Se_result
+
+        );
+    
+    SignOutChoser      : TwoToOne8BitMux
+    PORT MAP
+    ( 
+        SEL => AEGTBE,
+        A => SignA,
+        B => SignB,
+        X => SignOut,
+    );
+    
+    MantissaOut <= register_Bm_result;
+    ExponentOut <= register_Se_result(6 downto 0);
+
+
     END ARCHITECTURE;
