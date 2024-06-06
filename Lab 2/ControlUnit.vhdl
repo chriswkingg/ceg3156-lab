a81b0107389394842b6dbe902962730ae22e6269
Chriswkingg <chriswkingg@gmail.com>
2024-06-06 14:48:28 -0400

    Starting lab 2

---
+++ Lab 2/ControlUnit.vhdl
@@ -0,0 +1,14 @@
+LIBRARY ieee;
+USE  ieee.std_logic_1164.all;
+
+ENTITY ControlUnit is
+    PORT
+    (
+
+    );
+    END ControlUnit;
+
+ARCHITECTURE rtl of ControlUnit is
+BEGIN
+
+END ARCHITECTURE;
